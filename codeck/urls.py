8a4b9df1bc1a2b2c786a6e524332ca64d13fc0b8
berk-12 <36491172+berk-12@users.noreply.github.com>
2021-09-07 20:00:18 +0300

    Update main page url

--- codeck/urls.py
+++ codeck/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', include('movie.urls')),
+    path('', include('movie.urls')),
     path('api/', include('movie.api.urls')),
     path('api-auth', include('rest_framework.urls'))
 ]
